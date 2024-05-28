4a8723ad4b09a012493c1daea03276dd592b5d2b
GEORGE MYLES <146440695+44george@users.noreply.github.com>
2024-05-28 19:51:42 +0300

    Add files via upload

---
+++ divisibility test 11.py
@@ -0,0 +1,4 @@
+#divisibility test for number 11
+int(input("enter number"))
+divisibility_test=int("number%11")
+print("divisibilty_test")
---
+++ pay.py
@@ -0,0 +1,7 @@
+#calculating gross pay
+hours=int(input("Enter hours"))
+rate=int(input("Enter rate"))
+print(type("enter hours"))
+print(type("enter rate"))
+Gross_pay=int(hours*rate)
+print(Gross_pay)
