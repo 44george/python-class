0b13f19d471534ecc7e68a9dce1ace978891f51c
GEORGE MYLES <146440695+44george@users.noreply.github.com>
2024-05-28 20:40:36 +0300

    Delete loans.py

--- loans.py
+++
@@ -1,9 +0,0 @@
-#loan illegibility
-Name=(input("enter your Name"))
-Age=int(input("enter your Age"))
-Annual_income=int(input("enter your Annual_income"))
-print(type("Age"))
-print(type("Annual_income"))
-if Age>=21 and Annual_income>=21000:
-    loan=("loan granted")
-print("loan granted")
