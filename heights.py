5c80bc7ba2d4ad75d0ea59e93482bf6a2bf194c2
GEORGE MYLES <146440695+44george@users.noreply.github.com>
2024-05-28 20:44:24 +0300

    Add files via upload

---
+++ heights.py
@@ -0,0 +1,7 @@
+#compare heights
+John=int(input("enter john_height"))
+Alice=int(input("enter Alice_height"))
+Bob=int(input("enter Bob_height"))
+if Alice>John and Bob:
+    Tallest=Alice
+    print("Alice is the tallest")
